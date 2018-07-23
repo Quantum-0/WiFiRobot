4f7425852071ce7aeeb9bcfc06ffc2295be02fb2
Quantum <quantum0@mail.ru>
2018-07-23 15:43:40 +0300

    Upload Client part

---
+++ Управление машинкой/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Управление_машинкой
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelW = new System.Windows.Forms.Panel();
+            this.panelS = new System.Windows.Forms.Panel();
+            this.panelA = new System.Windows.Forms.Panel();
+            this.panelD = new System.Windows.Forms.Panel();
+            this.trackBarSpeed = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelW
+            // 
+            this.panelW.BackColor = System.Drawing.Color.Black;
+            this.panelW.Location = new System.Drawing.Point(85, 31);
+            this.panelW.Name = "panelW";
+            this.panelW.Size = new System.Drawing.Size(64, 64);
+            this.panelW.TabIndex = 0;
+            // 
+            // panelS
+            // 
+            this.panelS.BackColor = System.Drawing.Color.Black;
+            this.panelS.Location = new System.Drawing.Point(85, 101);
+            this.panelS.Name = "panelS";
+            this.panelS.Size = new System.Drawing.Size(64, 64);
+            this.panelS.TabIndex = 1;
+            // 
+            // panelA
+            // 
+            this.panelA.BackColor = System.Drawing.Color.Black;
+            this.panelA.Location = new System.Drawing.Point(15, 101);
+            this.panelA.Name = "panelA";
+            this.panelA.Size = new System.Drawing.Size(64, 64);
+            this.panelA.TabIndex = 2;
+            // 
+            // panelD
+            // 
+            this.panelD.BackColor = System.Drawing.Color.Black;
+            this.panelD.Location = new System.Drawing.Point(155, 101);
+            this.panelD.Name = "panelD";
+            this.panelD.Size = new System.Drawing.Size(64, 64);
+            this.panelD.TabIndex = 2;
+            // 
+            // trackBarSpeed
+            // 
+            this.trackBarSpeed.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBarSpeed.Location = new System.Drawing.Point(12, 204);
+            this.trackBarSpeed.Maximum = 150;
+            this.trackBarSpeed.Minimum = 40;
+            this.trackBarSpeed.Name = "trackBarSpeed";
+            this.trackBarSpeed.Size = new System.Drawing.Size(350, 45);
+            this.trackBarSpeed.TabIndex = 3;
+            this.trackBarSpeed.Value = 40;
+            this.trackBarSpeed.Scroll += new System.EventHandler(this.trackBarSpeed_Scroll);
+            this.trackBarSpeed.KeyDown += new System.Windows.Forms.KeyEventHandler(this.trackBarSpeed_KeyDown);
+            this.trackBarSpeed.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.trackBarSpeed_KeyPress);
+            this.trackBarSpeed.KeyUp += new System.Windows.Forms.KeyEventHandler(this.trackBarSpeed_KeyUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(293, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(69, 173);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBarSpeed);
+            this.Controls.Add(this.panelD);
+            this.Controls.Add(this.panelA);
+            this.Controls.Add(this.panelS);
+            this.Controls.Add(this.panelW);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelW;
+        private System.Windows.Forms.Panel panelS;
+        private System.Windows.Forms.Panel panelA;
+        private System.Windows.Forms.Panel panelD;
+        private System.Windows.Forms.TrackBar trackBarSpeed;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Управление машинкой/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Управление_машинкой
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        UdpClient client = new UdpClient("192.168.4.1", 100);
+        bool bF, bL, bR, bB, speedChanged;
+
+        private void trackBarSpeed_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Form1_KeyPress(sender, e);
+        }
+
+        private void trackBarSpeed_KeyUp(object sender, KeyEventArgs e)
+        {
+            Form1_KeyUp(sender, e);
+        }
+
+        private void trackBarSpeed_KeyDown(object sender, KeyEventArgs e)
+        {
+            Form1_KeyDown(sender, e);
+        }
+
+        private void trackBarSpeed_Scroll(object sender, EventArgs e)
+        {
+            speedChanged = true;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*
+            if (e.KeyChar == 'w')
+            {
+                panelW.BackColor = Color.LimeGreen;
+                bF = true;
+            }
+            if (e.KeyChar == 'a')
+            {
+                panelA.BackColor = Color.LimeGreen;
+                bL = true;
+            }
+            if (e.KeyChar == 's')
+            {
+                panelS.BackColor = Color.LimeGreen;
+                bB = true;
+            }
+            if (e.KeyChar == 'd')
+            {
+                panelD.BackColor = Color.LimeGreen;
+                bR = true;
+            }
+
+            UpdateRobot();
+            */
+        }
+
+        private void SetSpeed(char motor, int speed)
+        {
+            var data = Encoding.ASCII.GetBytes("_S_");
+            data[2] = (byte)(speed);
+            data[0] = (byte)motor;
+            client.Send(data, 3);
+            listBox1.Items.Add(motor + "S(" + speed + ")");
+            if (listBox1.Items.Count > 13)
+                listBox1.Items.RemoveAt(0);
+        }
+
+        private void SetMode(char motor, char mode)
+        {
+            var data = Encoding.ASCII.GetBytes(motor + "M" + mode);
+            client.Send(data, 3);
+            listBox1.Items.Add(Encoding.ASCII.GetString(data));
+            if (listBox1.Items.Count > 13)
+                listBox1.Items.RemoveAt(0);
+        }
+
+        private void Beep()
+        {
+            client.Send(Encoding.ASCII.GetBytes("BEP"), 3);
+            listBox1.Items.Add("BEP");
+            if (listBox1.Items.Count > 13)
+                listBox1.Items.RemoveAt(0);
+        }
+
+
+
+        private void UpdateRobot()
+        {
+            if (speedChanged)
+            {
+                SetSpeed('A', trackBarSpeed.Value);
+                speedChanged = false;
+                Task.Delay(250).Wait();
+                Beep(); Beep(); Beep();
+                Task.Delay(250).Wait();
+            }
+            if (bF)
+            {
+                if (bB)
+                {
+                    Beep();
+                    SetMode('A', 'R');
+                }
+                else if (!bL && !bR)
+                    SetMode('A', 'F');
+                else if (bL && bR)
+                    Beep();
+                /*else if (bL)
+                {
+                    var data = Encoding.ASCII.GetBytes("LS ");
+                    data[2] = (byte)(trackBarSpeed.Value + 5);
+                    client.Send(data, 3);
+                    data[0] = (byte)'R';
+                    data[2] = (byte)(trackBarSpeed.Value - 70);
+                    client.Send(data, 3);
+                    client.Send(Encoding.ASCII.GetBytes("AMF"), 3);
+                    //speedChanged = true;
+                }
+                else
+                {
+                    var data = Encoding.ASCII.GetBytes("RS ");
+                    data[2] = (byte)(trackBarSpeed.Value + 5);
+                    client.Send(data, 3);
+                    data[0] = (byte)'L';
+                    data[2] = (byte)(trackBarSpeed.Value - 70);
+                    client.Send(data, 3);
+                    client.Send(Encoding.ASCII.GetBytes("AMF"), 3);
+                    //speedChanged = true;
+                }*/
+            }
+            else
+            {
+                if (bB)
+                {
+                    SetMode('A', 'B');
+                }
+                else if (bL && bR)
+                {
+                    Beep();
+                }
+                else if (bL)
+                {
+                    SetMode('R', 'F');
+                    SetMode('L', 'B');
+                }
+                else if (bR)
+                {
+                    SetMode('L', 'F');
+                    SetMode('R', 'B');
+                }
+                else
+                {
+                    SetMode('A', 'R');
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                panelW.BackColor = Color.DarkGreen;
+                bF = false;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                panelA.BackColor = Color.DarkGreen;
+                bL = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                panelS.BackColor = Color.DarkGreen;
+                bB = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                panelD.BackColor = Color.DarkGreen;
+                bR = false;
+            }
+            if (e.KeyCode != Keys.W && e.KeyCode != Keys.A && e.KeyCode != Keys.S && e.KeyCode != Keys.D)
+                return;
+
+            UpdateRobot();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                if (bF)
+                    return;
+                panelW.BackColor = Color.LimeGreen;
+                bF = true;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                if (bL)
+                    return;
+                panelA.BackColor = Color.LimeGreen;
+                bL = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                if (bB)
+                    return;
+                panelS.BackColor = Color.LimeGreen;
+                bB = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                if (bR)
+                    return;
+                panelD.BackColor = Color.LimeGreen;
+                bR = true;
+            }
+            if (e.KeyCode != Keys.W && e.KeyCode != Keys.A && e.KeyCode != Keys.S && e.KeyCode != Keys.D)
+                return;
+
+            UpdateRobot();
+        }
+    }
+}
